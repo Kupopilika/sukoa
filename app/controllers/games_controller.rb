54143e0601cf5001e0a4517c175e17dfb589e5ef
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-06-16 21:16:25 +0200

    updated game controller and centered home page items

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,5 +4,14 @@ class GamesController < ApplicationController
   end
 
   def create
+    @game = Game.new(params[game_params])
+    @game.save
+    redirect_to edit_game_path(@game)
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :goal_score, :number, :player_id)
   end
 end
