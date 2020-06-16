812c950467751a965ebc2e7a4f60423e04496f2e
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-06-16 17:54:03 +0200

    added default value to user default score

--- db/migrate/20200616134649_create_players.rb
+++ db/migrate/20200616134649_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[6.0]
   def change
     create_table :players do |t|
       t.string :name
-      t.integer :current_score
+      t.integer :current_score, default: 0
       t.boolean :winner, default: false
       t.references :game, null: false, foreign_key: true
 
