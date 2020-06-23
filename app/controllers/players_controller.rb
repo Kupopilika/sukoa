0cd82912af93657577ac6b8e558c9e97b657ae86
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-06-23 16:30:02 +0200

    modified form for new player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,10 +7,7 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
     @player.game = Game.find(params[:game_id])
-    if @player.save
-    else
-      render 'create'
-    end
+    @player.save
   end
 
   private
