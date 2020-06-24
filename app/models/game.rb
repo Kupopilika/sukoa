c3c7f65f2adbe50644368bd3dba1d34d490ed511
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-06-24 11:41:00 +0200

    goal score greater than 0

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,5 @@ class Game < ApplicationRecord
   validates :name, presence: true
   validates :goal_score, presence: true
   validates :number, presence: true
+  validates :goal_score, numericality: { greater_than: 0 }
 end
